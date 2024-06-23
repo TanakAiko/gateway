3dc22d6474c4a952b756996cbd7ad7225445ce1d
TanakAiko <cherifmbaye02@gmail.com>
2024-06-23 23:13:06 +0000

    don't know

---
+++ internals/handlers/authorized.go
@@ -0,0 +1,69 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	md "gateway/model"
+	"io"
+	"net/http"
+)
+
+func AuthorizedHandler(w http.ResponseWriter, r *http.Request) {
+	const action = "login"
+
+	if r.Method != http.MethodPost {
+		http.Error(w, "methode not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	body, err := io.ReadAll(r.Body)
+	if err != nil {
+		fmt.Println("Bad request: " + err.Error())
+		http.Error(w, "Bad request: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	var user User
+
+	if err = json.Unmarshal(body, &user); err != nil {
+		http.Error(w, "Bad request: "+err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	bodyData := md.RequestBody{
+		Action: action,
+		Body:   user,
+	}
+
+	jsonData, err := json.Marshal(bodyData)
+	if err != nil {
+		http.Error(w, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	req, err := http.NewRequest("POST", URLauth, bytes.NewBuffer(jsonData))
+	if err != nil {
+		http.Error(w, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		http.Error(w, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Internal server error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(resp.StatusCode)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(responseBody)
+}
