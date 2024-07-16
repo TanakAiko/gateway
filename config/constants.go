7f974693126c933f3eb8efd1fc23d26593416238
TanakAiko <cherifmbaye02@gmail.com>
2024-07-16 16:08:24 +0000

    handle the message's stuff

--- config/constants.go
+++ config/constants.go
@@ -2,7 +2,7 @@ package config
 
 const URLauth = "http://authapi:8081"
 const URLPost = "http://postapi:8082"
-const URLChat = "http://localhost:8083"
+const URLChat = "http://chatapi:8083"
 const URLComment = "http://commentapi:8084"
 
 const Port = "8080"
